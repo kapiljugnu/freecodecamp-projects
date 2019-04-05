086682fe394c05c4c242194446d8199074793875
Kapil Jugnu <kr_kapil@hotmail.com>
2019-04-05 13:39:02 +0530

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        d3: './src/d3/index.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js'
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'd3.html',
+            title: 'D3 Projects',
+            chunks: ['d3']
+        })
+    ],
+    module: {
+        rules: [
+            { test: /\.js$/, use: 'babel-loader' }
+        ]
+    },
+    devServer: {
+        contentBase: './dist'
+    }
+}
