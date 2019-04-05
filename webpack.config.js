34c60f26c9fac669ee16d05239e710b66d94ae8f
Kapil Jugnu <kr_kapil@hotmail.com>
2019-04-05 15:10:05 +0530

    added axios

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,39 @@
-const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackExternalsPlugin= require('html-webpack-externals-plugin');
+const path = require("path");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackExternalsPlugin = require("html-webpack-externals-plugin");
 
 module.exports = {
-    entry: {
-        d3: './src/d3/index.js',
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js'
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            filename: 'd3.html',
-            title: 'D3 Projects',
-            chunks: ['d3']
-        }),
-        new HtmlWebpackExternalsPlugin({
-            externals: [{
-                module:'',
-                entry: 'https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js',
-            }]
-        })
-    ],
-    module: {
-        rules: [
-            { test: /\.js$/, use: 'babel-loader' }
-        ]
-    },
-    devServer: {
-        contentBase: './dist',
-        inline: true,
-        hot: true,
-    }
-}
+  entry: {
+    d3: "./src/d3/index.js"
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js"
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: "d3.html",
+      title: "D3 Projects",
+      chunks: ["d3"]
+    }),
+    new HtmlWebpackExternalsPlugin({
+      externals: [
+        {
+          module: "",
+          entry:
+            "https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js"
+        }
+      ]
+    })
+  ],
+  module: {
+    rules: [{ test: /\.js$/, use: "babel-loader" }]
+  },
+  devServer: {
+    contentBase: "./dist",
+    inline: true,
+    hot: true
+  }
+};
